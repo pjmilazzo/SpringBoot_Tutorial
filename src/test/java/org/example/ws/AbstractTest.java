03fa3567755416209183c65fb972779cb3c0b22d
Paul Milazzo <paul.j.milazzo@accenture.com>
2016-08-16 17:38:29 -0400

    Added some clean up for Spring Boot 1.4.0

--- src/test/java/org/example/ws/AbstractTest.java
+++ src/test/java/org/example/ws/AbstractTest.java
@@ -5,16 +5,17 @@ import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * The AbstractTest class is the parent of all JUnit test classes. This class
  * configures the test ApplicationContext and test runner environment.
  * 
- * @author Matt Warman
  */
-@RunWith(SpringJUnit4ClassRunner.class)
+//@RunWith(SpringJUnit4ClassRunner.class)
 //@SpringApplicationConfiguration(classes = Application.class)
+@RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)  
 public abstract class AbstractTest {
 
